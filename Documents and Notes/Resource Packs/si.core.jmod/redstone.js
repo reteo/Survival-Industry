037e16de3c638f1ac7fb98e33d75959ac2cf71f4
Reteo <reteo.varala@gmail.com>
2016-04-23 09:21:11 -0400

    Temporary fix for loop problem; added some additional recipes.

--- Documents and Notes/Resource Packs/si.core.jmod/redstone.js
+++ Documents and Notes/Resource Packs/si.core.jmod/redstone.js
@@ -1,55 +1,101 @@
-// Let's make some materials easier to read and reference.
+// Let's give some items easier names
 var pureCertusQuartz = "appliedenergistics2:item.ItemMultiMaterial:10"
 var pureNetherQuartz = "appliedenergistics2:item.ItemMultiMaterial:11"
+var camouflageBlock = "ExpandedRedstone:expanded redstone_item_placer:10"
+var chestReader = "ExpandedRedstone:expanded redstone_item_placer:7"
+var signalDriver = "ExpandedRedstone:expanded redstone_item_placer:8"
+var itemEffector = "ExpandedRedstone:expanded redstone_item_placer:3"
 
-// Altering Redstone tool recipes to remove iron requirements; uses multiple materials.
-var ingotMats = ["ignotAluminum", "ingotCopper", "ingotIron", "ingotBronze"];
+// Let's make a new oredict referencing structural-type metal ingots.
+//var ingotMats = ["ingotAluminum","ingotCopper","ingotIron","ingotBronze","ingotSteel","ingotHSLA","ingotNickel","ingotPlatinum"];
+//for (var x in ingotMats) addOreDict(x, "ingotMetal");
 
-// Piston Recipe.
-removeRecipes( "minecraft:piston");
-for(var currentIngot in ingotMats){
-	addShapedRecipe("minecraft:piston@1", [
-		["plankWood", "plankWood", "plankWood" ],
-		["cobblestone", currentIngot, "cobblestone" ],
-		["cobblestone", "dustRedstone", "cobblestone" ]]);
-}
+addOreDict("ingotAluminum", "ingotMetal");
+addOreDict("ingotCopper", "ingotMetal");
+addOreDict("ingotIron", "ingotMetal");
+addOreDict("ingotBronze", "ingotMetal");
+addOreDict("ingotSteel", "ingotMetal");
+addOreDict("ingotHSLA", "ingotMetal");
+addOreDict("ingotNickel", "ingotMetal");
+addOreDict("ingotPlatinum", "ingotMetal");
 
-// Tripwire Hook Recipe.
-removeRecipes( "minecraft:tripwire_hook");
-for(var currentIngot in ingotMats){
-	addShapedRecipe("minecraft:tripwire_hook@2", [
-		[currentIngot, null, null ],
-		["stickWood", null, null ],
-		["plankWood", null, null ]]);
-}
+// Let's make a new oredict referencing all types of quartz.
+//var quartzMats = ["gemQuartz","crystalCertusQuartz",pureCertusQuartz,pureNetherQuartz];
+//for (var x in quartzMats) addOreDict(x, "materialQuartz");
+
+addOreDict("gemQuartz", "materialQuartz");
+addOreDict("crystalCertusQuartz", "materialQuartz");
+addOreDict(pureCertusQuartz, "materialQuartz");
+addOreDict(pureNetherQuartz, "materialQuartz");
+
+// Remove original recipes.
+removeRecipes("minecraft:piston");
+removeRecipes("minecraft:tripwire_hook");
+removeRecipes("minecraft:hopper");
+removeRecipes("minecraft:comparator");
+removeRecipes("minecraft:daylight_detector");
+removeRecipes(camouflageBlock);
+removeRecipes(chestReader);
+removeRecipes(signalDriver);
+removeRecipes(itemEffector);
+
+// Make new recipes:
+// Piston.
+addShapedRecipe("minecraft:piston", [
+	["plankWood", "plankWood", "plankWood" ],
+	["cobblestone", "ingotMetal", "cobblestone" ],
+	["cobblestone", "dustRedstone", "cobblestone" ]]);
+
+
+// Tripwire Hook.
+addShapedRecipe("minecraft:tripwire_hook@2", [
+	["ingotMetal", null, null ],
+	["stickWood", null, null ],
+	["plankWood", null, null ]]);
+
 
 // Hopper.
-removeRecipes( "minecraft:hopper");
-for(var currentIngot in ingotMats){
-	addShapedRecipe("minecraft:hopper@1", [
-		[null, null, currentIngot ],
-		[currentIngot, "minecraft:chest", currentIngot ],
-		[null, currentIngot, null ]]);
-}
-
-
-// Also adding Certus Quartz to recipes using quartz.
-var quartzMats = ["gemQuartz", "crystalCertusQuartz", pureCertusQuartz, pureNetherQuartz];
+addShapedRecipe("minecraft:hopper", [
+	[null, null, "ingotMetal" ],
+	["ingotMetal", "minecraft:chest", "ingotMetal" ],
+	[null, "ingotMetal", null ]]);
 
 // Redstone Comparator.
-removeRecipes( "minecraft:comparator");
-for(var currentQuartz in quartzMats){
-	addShapedRecipe("minecraft:comparator@1", [
-		[null, "minecraft:redstone_torch", null ],
-		["minecraft:redstone_torch", currentQuartz, "minecraft:redstone_torch" ],
-		["stone", "stone", "stone" ]]);
-}
+addShapedRecipe("minecraft:comparator", [
+	[null, "minecraft:redstone_torch", null ],
+	["minecraft:redstone_torch", "materialQuartz", "minecraft:redstone_torch" ],
+	["stone", "stone", "stone" ]]);
 
 // Daylight Sensor.
-removeRecipes( "minecraft:daylight_detector");
-for(var currentQuartz in quartzMats){
-	addShapedRecipe("minecraft:comparator@1", [
-		["blockGlass", "blockGlass", "blockGlass" ],
-		[currentQuartz, currentQuartz, currentQuartz ],
-		["slabWood", "slabWood", "slabWood" ]]);
-}
+addShapedRecipe("minecraft:daylight_detector", [
+	["blockGlass", "blockGlass", "blockGlass" ],
+	["materialQuartz", "materialQuartz" ,"materialQuartz" ],
+	["slabWood", "slabWood", "slabWood" ]]);
+
+// Camouflage Block.
+addShapedRecipe(camouflageBlock, [
+	["dustRedstone", "ingotMetal", "dustRedstone" ],
+	["ingotMetal", "pearlEnderEye" ,"ingotMetal" ],
+	["dustRedstone", "ingotMetal", "slabWood" ]]);
+
+// Chest Reader.
+addShapedRecipe(chestReader, [
+	[null, "chest", null ],
+	["dustRedstone", "materialQuartz" ,"dustRedstone" ],
+	[null, "slabStone", null ]]);
+
+// Signal Driver.
+addShapedRecipe(signalDriver, [
+	[null, "slabStone", null ],
+	["dustRedstone", "materialQuartz" ,"dustRedstone" ],
+	[null, "slabStone", null ]]);
+
+// Item Effector
+addShapedRecipe(signalDriver, [
+	["cobblestone", "cobblestone", "cobblestone" ],
+	["materialQuartz", "minecraft:dispenser" ,"pearlEnder" ],
+	["cobblestone", "dustRedstone", "cobblestone" ]]);
+
+
+
+
