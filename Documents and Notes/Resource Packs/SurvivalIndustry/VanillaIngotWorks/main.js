a058887a3bbddeacf450b44d459706e1e6821b16
Reteo <reteo.varala@gmail.com>
2016-07-02 20:19:02 -0400

    Fixed and refined the Blast Furnace Recipes.

--- Documents and Notes/Resource Packs/SurvivalIndustry/VanillaIngotWorks/main.js
+++ Documents and Notes/Resource Packs/SurvivalIndustry/VanillaIngotWorks/main.js
@@ -1,44 +1,63 @@
 /* Vanilla Ingot Works: Allowing ore doubling and alloying support without the need for additional machines. */
 
-
 // Makes a custom creative tab for custom items.
 addCreativeTab("VanillaIngotWorks.general","Vanilla Ingot Works","VanillaIngotWorks:itemCrucible");
+
 
 // Before we can do anything, let's make the basic tools of the trade.
 addItem("itemCrucible", "ItemCrucible", 64, "VanillaIngotWorks.general");
 addItem("itemIngotMold", "ItemIngotMold", 64, "VanillaIngotWorks.general");
 
+
 // Next, we need the recipes for the above tools.
 addShapelessRecipe("VanillaIngotWorks:itemIngotMold", ["minecraft:sand"]);
 addShapelessRecipe("VanillaIngotWorks:itemCrucible", ["minecraft:clay","minecraft:sand"]);
+
 
 // This restores the sand used for an unneeded mold by crafting it with a stick.
 addShapelessRecipe("minecraft:stick", ["VanillaIngotWorks:itemIngotMold","minecraft:stick"]);
 
 
-// Define the different materials we'll be using.  For now, we'll just add common metals. 
-var ores = ["Iron","Gold","Copper","Tin","Aluminum","Silver", "Lead", "Nickel", "Platinum"];
+// Define the different materials we'll be using. 
+var ores = [["Iron",    null],
+            ["Gold",    null],
+            ["Copper",  null],
+            ["Tin",     null],
+            ["Aluminum",null],
+            ["Silver",  null],
+            ["Lead",    null],
+            ["Nickel",  null],
+            ["Platinum",null]
+];
+
 
 // Now, we define the basic alloys we'll most likely use.
-// Format:	"First Material", "Second Material", "Alloy"
+// Legend:	Result, [Ingredient 1, Ingredient 2]
 var alloys = [
-	["Copper","Tin","Bronze"],
-	["Silver","Gold","Electrum"]];
+	["Bronze",      ["Copper","Tin"]],
+	["Electrum",    ["Silver","Gold"]]
+];
 
 
 // First, alloy ingots with ingots to make 2 ingots each.
 for(var m in alloys){
-	addAlloy("ingot" + alloys[m][0], "ingot" + alloys[m][1], "ingot" + alloys[m][2], 2);
+	addAlloy("ingot" + alloys[m][1][0], "ingot" + alloys[m][1][1], "ingot" + alloys[m][0], 2);
 }
+
 
 // Next, alloy ores with ores to make 4 ingots each.
 for(var m in alloys){
-	addAlloy("ore" + alloys[m][0], "ore" + alloys[m][1], "ingot" + alloys[m][2], 4);
+	addAlloy("ore" + alloys[m][1][0], "ore" + alloys[m][1][1], "ingot" + alloys[m][0], 4);
 }
+
 
 // Finally, duplicate ores.
 for(var m in ores){
-	addAlloy("ore" + ores[m], "ingot" + ores[m], 2);
+    if ((ores[m][1] == null) || isModLoaded(ores[m][1]))
+        addAlloy("ore" + ores[m][0], "ingot" + ores[m][0], 2);
 }
 
-load("SurvivalIndustry.js");
+
+// Add Blast Furnace alloying recipes if RotaryCraft is installed.
+if (isModLoaded("RotaryCraft"))
+    load("RotaryCraft.js");
