40f19ecb83baa6904887e7936e4894d246f2fb0b
Lampros Liontos <reteo.varala@gmail.com>
2017-12-12 13:56:43 -0500

    Simplified ore recipes code; still isn't working right now.

--- mods/JMOD Scripts/ExMachina/ore-recipes.js
+++ mods/JMOD Scripts/ExMachina/ore-recipes.js
@@ -13,68 +13,58 @@ var oreStyles = ["Gravel", "Sand", "Dust"];
 // First, let's do basic ore duplication.
 
 // Native first.
+log("Native ores:")
 for (var m in metalsNative){
     for (var o in oreStyles) {
-	oreMetal = metalsNative[m];
-	oreStyle = oreStyles[o];
-	metalType = oreMetal.toLowerCase();
-	metalStyle = oreStyle.toLowerCase();
-	
-	VIW.addAlloy("exnihilo:" + metalType + "_" + metalStyle, "ingot" + oreMetal, 2);
-	log("addAlloy Arguments: " + "exnihilo:" + metalType + "_" + metalStyle + ", " + "ingot" + oreMetal + ", " +  "2");
+	nativeOreDoubling(metalsNative[m],oreStyles[o]);
     }
 }
+
+// Nether next.
+for (var m in metalsNether){
+    for (var o in oreStyles) {
+	nativeOreDoubling(metalsNether[m],oreStyles[o]);
+    }
+}
+
+// Then, the End.
+for (var m in metalsEnder){
+    for (var o in oreStyles) {
+	nativeOreDoubling(metalsEnder[m],oreStyles[o]);
+    }
+}
+
 
 // Now for AOBD ores.
 for (var m in metalsAobd){
     for (var o in oreStyles) {
-	metalType = metalsAobd[m];
-	metalStyle = oreStyles[o];
-	
-	VIW.addAlloy("aobd:ore" + metalStyle + metalType, "ingot" + metalType, 2);
-	log("addAlloy Arguments: " + "aobd:ore" + metalStyle + metalType + ", " +  "ingot" + metalType + ", " +  "2");
-    }
-}
-    
-// For gravel, there are nether and ender variants.
-
-// First, Native Nether ores.
-for (var m in metalsNether){
-    for (var o in oreStyles) {
-	oreMetal = metalsNether[m];
-	oreStyle = oreStyles[o];
-	metalType = oreMetal.toLowerCase();
-	metalStyle = oreStyle.toLowerCase();
-	
-	VIW.addAlloy("exnihilo:" + metalType + "_" + metalStyle, "ingot" + oreMetal, 2);
-	log("addAlloy Arguments: " + "exnihilo:" + metalType + "_" + metalStyle + ", " +  "ingot" + oreMetal + ", " +  "2");
+	aobdOreDoubling(metalsAobd[m], oreStyles[o]);
     }
 }
 
 // Then, AOBD Nether ores.
 for (var m in metalsNetherAobd){
     for (var o in oreStyles) {
-	metalType = metalsNetherAobd[m];
-	metalStyle = oreStyles[o];
-	
-	VIW.addAlloy("aobd:ore" + metalStyle + metalType, "ingot" + metalType, 2);
-	log("addAlloy Arguments: " + "aobd:ore" + metalStyle + metalType + ", " +  "ingot" + metalType + ", " +  "2");
-    }
-}
-
-// Finally, Ender ores (there are no AOBD variants for ender ores)
-for (var m in metalsEnder){
-    for (var o in oreStyles) {
-	oreMetal = metalsEnder[m];
-	oreStyle = oreStyles[o];
-	metalType = oreMetal.toLowerCase();
-	metalStyle = oreStyle.toLowerCase();
-	
-	VIW.addAlloy("exnihilo:" + metalType + "_" + metalStyle, "ingot" + oreMetal, 2);
-	log("addAlloy Arguments: " + "exnihilo:" + metalType + "_" + metalStyle + ", " +  "ingot" + oreMetal + ", " +  "2");
+	aobdOreDoubling(metalsNetherAobd[m], oreStyles[o]);
     }
 }
 
 /* We want to be able to use the extractor, but vanilla ores aren't available in skyblock.
    To fix this, we can simply add recipes to make ore blocks out of broken ores. */
 
+
+
+/* Functions */
+
+function nativeOreDoubling(oreMetal, oreStyle) {
+    metalType = oreMetal.toLowerCase();
+    metalStyle = oreStyle.toLowerCase();
+
+    log("Metal: " + oreMetal + " - Form: " + oreStyle);
+    VIW.addAlloy("ingot" + oreMetal, "exnihilo:" + metalType + "_" + metalStyle, 2);
+}
+
+function aobdOreDoubling(oreMetal, oreStyle) {
+    log("Metal: " + oreMetal + " - Form: " + oreStyle);
+    VIW.addAlloy("ingot" + oreMetal, "aobd:ore" + oreStyle + oreMetal, 2);
+}
